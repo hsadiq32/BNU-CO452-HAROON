8cc07bdc4ee5767ad70747752b16fbf3c4a2e7ae
hsadiq32 <72034904+hsadiq32@users.noreply.github.com>
2020-10-07 17:28:00 +0100

    Added a Course Class

---
+++ app01-lab-classes/Course.java
@@ -0,0 +1,29 @@
+
+/**
+ * The Course class will be used to contain a code number and a title, e.g. G400, Computing..
+ *
+ * @author Haroon Sadiq
+ * @version 07/10/2020
+ */
+public class Course
+{
+    private String course_number;
+    private String course_title;
+    /**
+     * Constructor for creating objects of class Course
+     */
+    public Course(String course_number,String course_title)
+    {
+        this.course_number = course_number;
+        this.course_title = course_title;
+    }
+    
+    /**
+     * Prints out course_title and course_number to the IDE interface
+     */
+    public void print ()
+    {
+        System.out.println("Course: " + course_number + ", " + course_title);
+    }
+    
+}

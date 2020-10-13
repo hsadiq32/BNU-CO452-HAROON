54bdf08f5864d3162d618b210247b9bf58b5a94b
hsadiq32 <72034904+hsadiq32@users.noreply.github.com>
2020-10-13 19:21:19 +0100

    Created enum class

--- app02-ticketmachine/Coin.java
+++ app02-ticketmachine/Coin.java
@@ -2,10 +2,32 @@
 /**
  * Enumeration class Coin - write a description of the enum class here
  *
- * @author (your name here)
- * @version (version number or date here)
+ * @author Harooon Sadiq
+ * @version 11/10/2020
  */
-public enum Coin
-{
-    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
-}
+    public enum Coin
+    {   
+        // The set coins which will be used by the ticket machine
+        P10 (10),
+        P20 (20),
+        P100 (100),
+        P200 (200);
+
+        private final int value;
+
+        /**
+         * The constructor method
+         */
+        private Coin(int value)
+        {
+            this.value = value;
+        }
+
+        /**
+         * Fetches the value of the coin
+         */
+        public int getValue()
+        {
+            return value;
+        }
+     }

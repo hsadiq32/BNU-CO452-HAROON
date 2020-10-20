4545b69299d0fc266d0a48de0b2c530efbe5fe18
hsadiq32 <72034904+hsadiq32@users.noreply.github.com>
2020-10-20 05:31:56 +0100

    Added basic grade data on creation of course

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -36,6 +36,7 @@ public class Course
         capacityCounter = 0;
         credits = 0;
         overallMark = 0;
+        grade = "Pending";
         moduleArray = new ArrayList<Module>();
     }
 

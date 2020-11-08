d9ec5d91fdfa372b68183eabadb2aae57ad15d67
hsadiq32 <72034904+hsadiq32@users.noreply.github.com>
2020-11-08 17:42:57 +0000

    Updated descriptors

--- app04-products/StockDemo.java
+++ app04-products/StockDemo.java
@@ -1,19 +1,22 @@
+import java.util.*;
 /**
  * Demonstrate the StockManager and Product classes.
  * The demonstration becomes properly functional as
  * the StockManager class is completed.
  * 
  * @author David J. Barnes and Michael Kölling.
+ * @modified Haroon Sadiq
  * @version 2016.02.29
  */
 public class StockDemo
 {
-    // The stock manager.
+    // The StockManager class dependencies.
     private StockManager manager;
-
+    // Allows the demo to randomise structured test data
+    private Random rand = new Random();
     /**
      * Create a StockManager and populate it with a few
-     * sample products.
+     * sample products inside an arraylist with manager dependencies
      */
     public StockDemo(StockManager manager)
     {
@@ -22,19 +25,20 @@ public class StockDemo
         manager.addProduct(new Product(13, "GoPro HERO9 Black"));
         manager.addProduct(new Product(14, "Apple Watch Series 6"));
         manager.addProduct(new Product(15, "Oculus Quest 2"));
-        manager.addProduct(new Product(16, "2020 iPad Air"));
+        manager.addProduct(new Product(16, "Dell 22-Inch Full HD Monitor"));
         manager.addProduct(new Product(17, "Canon EOS R5"));
         manager.addProduct(new Product(18, "Samsung 49-Inch Curved Gaming Monitor"));
         manager.addProduct(new Product(19, "SanDisk 1TB SD Card"));
         manager.addProduct(new Product(20, "Microsoft Surface Headphones 2"));
         manager.addProduct(new Product(21, "Fujifilm X100V"));
-        manager.addProduct(new Product(22, "iPhone SE"));
+        manager.addProduct(new Product(22, "iPhone X"));
     }
     
     /**
      * Provide a very simple demonstration of how a StockManager
-     * might be used. Details of one product are shown, the
-     * product is restocked, and then the details are shown again.
+     * might be used. Details of one product are shown
+     * Uses a for loop and iterates ID to go through all data
+     * Gives a range of 1-12 new stock
      */
     public void demoDelivery()
     {
